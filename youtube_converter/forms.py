8338a7c72ff8bfd46b37e7ad378e9362c3d97083
akyl0221 <33193159+akyl0221@users.noreply.github.com>
2019-05-29 22:29:48 +0600

    Add .idea to gitignore and url validation

--- youtube_converter/forms.py
+++ youtube_converter/forms.py
@@ -3,4 +3,5 @@ from django import forms
 
 class UrlForm(forms.Form):
     url = forms.URLField(
+        regex=r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$',
         widget=forms.TextInput(attrs={'placeholder': 'Paste your Youtube link'}), max_length=300)
