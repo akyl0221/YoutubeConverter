8f7d72860c67e707760655c8c62a1e27422ac800
akyl0221 <33193159+akyl0221@users.noreply.github.com>
2019-05-29 22:38:36 +0600

    REgex

--- youtube_converter/forms.py
+++ youtube_converter/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class UrlForm(forms.Form):
-    url = forms.URLField(
+    url = forms.RegexField(
         regex=r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$',
         widget=forms.TextInput(attrs={'placeholder': 'Paste your Youtube link'}), max_length=300)
